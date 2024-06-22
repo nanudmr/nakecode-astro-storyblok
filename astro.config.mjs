7668a7ccc2d780328ba5840af795931b459c5e46
Eduardo De Matos <nanudmr@gmail.com>
2024-06-22 13:04:10 -0400

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ const env = loadEnv('', process.cwd(), 'STORYBLOK')
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nakecode.com',
   integrations: [
     storyblok({
       accessToken: env.STORYBLOK_TOKEN,
